b2679d62f1495b3645abacbb196d177bb462f8f8
Pragun Khera <pragun08@gmail.com>
2016-11-03 20:10:19 +0530

    Script updated to run from within scratch

--- Re-Run.sh
+++ Re-Run.sh
@@ -4,8 +4,19 @@ declare -a nStats=(5 10 15 20 25 30 35 40 45 50)
 echo ${nStats[*]}
 for i in "${nStats[@]}"
 do
-echo "Running for nStats =" $i
+	echo "Running for nStats =" $i
 
-(cd ../../; pwd)
-./waf --run 'RAA_TCP --nStas='$i
+	result=${PWD##*/}
+	actual="RAA_TCP"
+
+	if [ "$result" = "$actual" ]; then
+		cd ..
+		cd ..
+	
+	else
+		(cd ../../; pwd);
+		
+	fi
+	
+	./waf --run 'RAA_TCP --nStas='$i
 done	
--- run1-50.sh
+++ run1-50.sh
@@ -2,5 +2,6 @@
 for i in {1..50}
 do
 echo "Running for nStats =" $i
+(cd ../../; pwd)
 ./waf --run 'RAA_TCP --nStas='$i
 done	
