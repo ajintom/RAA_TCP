7e22b4412d5f3f0183d013ac97f06587a480ca26
Ajin Tom <ajinjtom@gmail.com>
2016-11-04 00:44:23 +0530

    Added bash file to run all RAAs for nStas 1 to 50

---
+++ Run_RAAs_1-50.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+echo "Initialising and printing nStats array"
+declare -a RAA=(AparfWifiManager AmrrWifiManager AarfWifiManager AarfcdWifiManager CaraWifiManager IdealWifiManager OnoeWifiManager MinstrelHtWifiManager ConstantRateWifiManager)
+echo ${RAA[*]}
+for i in "${RAA[@]}"
+do
+	echo "Running RAA =" $i
+	for j in {1..50}
+	do
+		echo "nStas=" $j
+		./waf --run 'RAA_TCP --RateManager='$i' --nStas='$j
+	done	
+done	
--- run1-50.sh
+++ run1-50.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
-for i in {1..50}
-do
-echo "Running for nStats =" $i
-(cd ../../; pwd)
-./waf --run 'RAA_TCP --nStas='$i
-done	
+
+echo "RAAs to run tests:"
+declare -a RAA=(AparfWifiManager AmrrWifiManager);
+echo ${RAA[*]}
+for i in ${RAA[@]}
+	do
+	echo "Running RAA =" $i
+		for j in {1..2}
+		do
+		echo "Running for nStats =" $j
+#(cd ../../; pwd)
+		./waf --run 'RAA_TCP --nStas='$j '--RateManager='$i
+		done	
+
+done
