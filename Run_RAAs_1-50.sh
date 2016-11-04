51dbdd475f5ede7cd3838f0a0a0a04f92fdb4564
pragunkhera <pragun08@gmail.com>
2016-11-04 21:07:45 +0530

    Updated scripts can now be called from anywhere

--- Run_RAAs_1-50.sh
+++ Run_RAAs_1-50.sh
@@ -3,13 +3,14 @@ echo "Initialising and printing nStats array"
 declare -a RAA=(AparfWifiManager AmrrWifiManager AarfWifiManager AarfcdWifiManager CaraWifiManager IdealWifiManager OnoeWifiManager MinstrelHtWifiManager ConstantRateWifiManager)
 #declare -a RAA=(ConstantRateWifiManager)
 echo ${RAA[*]}
+cd $(dirname "${BASH_SOURCE[0]}")
 cd ..
 cd ..
 for i in "${RAA[@]}"
 do
-	echo "Running RAA =" $i
 	for j in {1..50}
 	do
+		echo "Running RAA =" $i
 		echo "nStas=" $j
 		./waf --run 'RAA_TCP --RateManager='$i' --nStas='$j
 	done	
--- run1-50.sh
+++ run1-50.sh
@@ -3,13 +3,15 @@
 echo "RAAs to run tests:"
 declare -a RAA=(AparfWifiManager AmrrWifiManager);
 echo ${RAA[*]}
+cd $(dirname "${BASH_SOURCE[0]}")
+cd ..
+cd ..
 for i in ${RAA[@]}
 	do
 	echo "Running RAA =" $i
 		for j in {1..2}
 		do
 		echo "Running for nStats =" $j
-#(cd ../../; pwd)
 		./waf --run 'RAA_TCP --nStas='$j '--RateManager='$i
 		done	
 
