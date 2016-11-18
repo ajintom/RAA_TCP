9eee81776722b8ee62aa5a0592b269fe6c80a0b8
pragunkhera <pragun08@gmail.com>
2016-11-18 20:34:56 +0530

    Removed redundant scripts

--- Run_RAAs_1-50.sh
+++ Run_RAAs_1-50.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 echo "Initialising and printing nStats array"
-declare -a RAA=(AparfWifiManager AmrrWifiManager AarfWifiManager AarfcdWifiManager CaraWifiManager IdealWifiManager OnoeWifiManager MinstrelHtWifiManager ConstantRateWifiManager)
-#declare -a RAA=(ConstantRateWifiManager)
+#declare -a RAA=(AparfWifiManager AmrrWifiManager AarfWifiManager AarfcdWifiManager CaraWifiManager IdealWifiManager OnoeWifiManager MinstrelHtWifiManager ConstantRateWifiManager)
+declare -a RAA=(IdealWifiManager)
 echo ${RAA[*]}
 cd $(dirname "${BASH_SOURCE[0]}")
 cd ..
 cd ..
 for i in "${RAA[@]}"
 do
-	for j in {42..47}
+	for j in {16..50}
 	do
 		echo "Running RAA =" $i
 		echo "nStas=" $j
-		./waf --run 'RAA_TCP --RateManager='$i' --nStas='$j
+		./waf --cwd=Results --run 'RAA_TCP --RateManager='$i' --nStas='$j
 	done	
-done	
+done
