f13639bda0c00b502e47bc40b01b13d731c23b0b
pragunkhera <pragun08@gmail.com>
2016-11-18 20:59:09 +0530

    Restored ns3 code

--- wifi-major-analysis.cc
+++ wifi-major-analysis.cc
@@ -75,9 +75,8 @@ NS_LOG_COMPONENT_DEFINE ("wifi-major-dcatxop");
 
 using namespace ns3;
 uint32_t mac_drop = 0;
-Ptr<PacketSink> apSink, staSink;                         /* Pointer to the packet sink application */  
+Ptr<PacketSink> sink;                         /* Pointer to the packet sink application */           
 uint64_t lastTotalRx = 0;                     /* The value of the last total received bytes */
-ApplicationContainer sinkApps;
 
 
 // calculate throughput
@@ -110,8 +109,8 @@ int main (int argc, char *argv[])
   std::string dataRate = "10Mbps";                   /* Application layer datarate. */
   std::string tcpVariant = "ns3::TcpNewReno";        /* TCP variant type. */
   std::string phyRate = "HtMcs7";                    /* Physical layer bitrate. */
-  std::string RateManager = "OnoeWifiManager";
-  double simulationTime = 3;                        /* Simulation time in seconds. */
+  std::string RateManager = "MinstrelHtWifiManager";
+  double simulationTime = 100;                        /* Simulation time in seconds. */
   bool pcapTracing = false;                          /* PCAP Tracing is enabled or not. */
     
  //LogComponentEnable("DcaTxop", LOG_LEVEL_ALL);
@@ -154,9 +153,9 @@ int main (int argc, char *argv[])
   wifiPhy.Set ("TxPowerStart", DoubleValue (10.0));
   wifiPhy.Set ("TxPowerEnd", DoubleValue (10.0));
   wifiPhy.Set ("TxPowerLevels", UintegerValue (1));
-  wifiPhy.Set ("TxGain", DoubleValue (10));                      
-  wifiPhy.Set ("RxGain", DoubleValue (10));                      
-  wifiPhy.Set ("RxNoiseFigure", DoubleValue (-10));              
+  wifiPhy.Set ("TxGain", DoubleValue (10));                      // change? 
+  wifiPhy.Set ("RxGain", DoubleValue (10));                      // change?
+  wifiPhy.Set ("RxNoiseFigure", DoubleValue (-10));              // change?
   wifiPhy.Set ("CcaMode1Threshold", DoubleValue (-79));
   wifiPhy.Set ("EnergyDetectionThreshold", DoubleValue (-79 + 3));
   //wifiPhy.SetErrorRateModel ("ns3::YansErrorRateModel");
@@ -180,34 +179,17 @@ int main (int argc, char *argv[])
   //wifiHelper.SetRemoteStationManager ("ns3::MinstrelHtWifiManager");
   //wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager");
   
-  NodeContainer p2pNodes;
-  p2pNodes.Create (2);
-
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("1ms"));
-
-  NetDeviceContainer p2pDevices;
-  p2pDevices = pointToPoint.Install (p2pNodes);
-
+  NodeContainer backboneNodes;
   NetDeviceContainer backboneDevices;
 
   InternetStackHelper stack;
   CsmaHelper csma;
-  
-   
-  NodeContainer backboneNodes = p2pNodes.Get (0);
-  NodeContainer ServerNode = p2pNodes.Get (1);
-
-  stack.Install (ServerNode);
   Ipv4AddressHelper ip;
-  ip.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces;
-  p2pInterfaces = ip.Assign (p2pDevices);
-  ip.SetBase ("10.1.3.0", "255.255.255.0");
+  ip.SetBase ("192.168.0.0", "255.255.255.0");
+
   backboneNodes.Create (nWifis);
   stack.Install (backboneNodes);
-  backboneDevices = csma.Install (backboneNodes);  
+  backboneDevices = csma.Install (backboneNodes);            // why csma only on backbone? 
 
   double wifiX = 0.0;
 
@@ -245,12 +227,12 @@ int main (int argc, char *argv[])
       NetDeviceContainer bridgeDev;
       bridgeDev = bridge.Install (backboneNodes.Get (0), NetDeviceContainer (apDev, backboneDevices.Get (0)));     // why is this needed
 
-      //assign ip address to ap
+      // assign AP IP address to bridge, not wifi
       apInterface = ip.Assign (bridgeDev);
 
       // setup the STAs
       stack.Install (sta);
-      
+                                
       mobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator", // allocates random position within a disc
                                      "X", DoubleValue (0.0), 
                                      "Y", DoubleValue (0.0),
@@ -259,7 +241,7 @@ int main (int argc, char *argv[])
                                  
       mobility.Install (sta);
       wifiMac.SetType ("ns3::StaWifiMac",
-                       "Ssid", SsidValue (ssid));                
+                       "Ssid", SsidValue (ssid));                // why does station need SSID ?
       staDev = wifiHelper.Install (wifiPhy, wifiMac, sta); 
 
     Ptr<DcaTxop> dca[nStas];
@@ -285,41 +267,23 @@ int main (int argc, char *argv[])
       /* Populate routing table */
       Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
-  /* Install Downlink Traffic */
-  for(uint32_t i = 0; i< nStas; i++)
-  {
-    BulkSendHelper source ("ns3::TcpSocketFactory",
-                          InetSocketAddress(staInterface.GetAddress (i), 5555));
-    source.SetAttribute ("MaxBytes", UintegerValue (0));
-    ApplicationContainer sourceApps = source.Install (p2pNodes.Get(1));
-    sourceApps.Start(Seconds (0.0));
-    sourceApps.Stop(Seconds (10.0));
-  }
-  PacketSinkHelper sink ("ns3::TcpSocketFactory",
-                        InetSocketAddress (apInterface.GetAddress (0), 5555));
-  sinkApps = sink.Install (sta);
-  sinkApps.Start (Seconds (0.0));
-  sinkApps.Stop (Seconds (10.0));
 
-
-  /* upload traffic */
-  PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny(), 8080));  
+  PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny(), 9));  // mismatch between this and the next line? 
   ApplicationContainer sinkApp = sinkHelper.Install (backboneNodes.Get (0));
-  apSink = StaticCast<PacketSink> (sinkApp.Get (0));
-  
+  sink = StaticCast<PacketSink> (sinkApp.Get (0));
 
-  OnOffHelper server ("ns3::TcpSocketFactory", (InetSocketAddress (apInterface.GetAddress (0), 8080)));
+  OnOffHelper server ("ns3::TcpSocketFactory", (InetSocketAddress (apInterface.GetAddress (0), 9)));
   server.SetAttribute ("PacketSize", UintegerValue (payloadSize));
   server.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
   server.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
   server.SetAttribute ("DataRate", DataRateValue (DataRate (dataRate)));
-  
+
     for (uint32_t i = 0; i < nStas ; ++i)
   {
     ApplicationContainer serverApp = server.Install (sta.Get(i));
     serverApp.Start (Seconds (1.0)); 
   }
- 
+   
   sinkApp.Start (Seconds (0.0));
   //Simulator::Schedule (Seconds (1.1), &CalculateThroughput);
      
@@ -418,7 +382,7 @@ int main (int argc, char *argv[])
   myfile.close();
   
     
-  double averageThroughput = ((apSink->GetTotalRx() * 8) / (1e6  * simulationTime));
+  double averageThroughput = ((sink->GetTotalRx() * 8) / (1e6  * simulationTime));
   std::cout << "\nAverage throughtput: " << averageThroughput << " Mbit/s" << std::endl;
   return 0;
 }
